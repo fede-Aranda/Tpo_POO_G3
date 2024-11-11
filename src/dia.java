5f273d11959f2d7eda776692202fb1e98140116b
Federico Aranda <104226174+fede-Aranda@users.noreply.github.com>
2024-11-11 18:21:59 -0300

    Primera implementacion de la clase horario y el uso de arrays booleanos para representar la ocupacion de eventos, ademas de funciones privadas de soporte

--- src/dia.java
+++ src/dia.java
@@ -1,6 +1,6 @@
-public class dia extends fecha{
+public class dia extends fecha {
     private evento[] eventos;
-    private boolean[] horario //array de 24hs . 4 (1/4 de hora)
+    //private boolean[] horario; //array de 24hs . 4 (1/4 de hora)
+    private boolean[] horario;
     // ... constructores y getters/setters ...
 }
-
